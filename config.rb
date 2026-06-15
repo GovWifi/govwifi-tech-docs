370d1f48d0309fa56a6ae760c5d81b999bf7e256
Iram Shehzadi <Shehzadi193@gmail.com>
2026-06-15 21:10:06 +0100

    change report problem url

--- config.rb
+++ config.rb
@@ -6,6 +6,13 @@ set :markdown_engine, :kramdown
 set :markdown, input: 'GFM'
 set :relative_links, true
 
+# Point the contribution banner's "Report problem" link at the GovWifi admin
+# help page instead of the (broken) default GitHub "new issue" URL. The gem
+# reads config[:source_urls][:report_issue_url] and appends ?body=...&subject=...
+set :source_urls, {
+  report_issue_url: "https://admin.wifi.service.gov.uk/help"
+}
+
 configure :build do
     Middleman::Extensions::MinifyJavascript.config.setting(:compressor).value = proc {
         require 'uglifier'
